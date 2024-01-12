846fefefc3add821b77f7a414ed284f003ab1cea
djcho <eowns36@gmail.com>
2024-01-12 11:30:39 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/articles/create', function () {
+    return view('articles/create');
+});
+
+Route::post('/articles', function(Request $request){
+    //비어있지 않고, 문자열이고, 255자 제한
+    $request->validate([
+        'body' => [
+            'required',
+            'string',
+            'max:255'
+        ],
+    ]);
+    return 'hello';
 });
